b3ba9c68e0a496071d6b453e9d776ac3323e0064
ZhouYuantian <yzhou5@student.rccd.edu>
2023-05-08 22:05:32 -0700

    finish

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -5,13 +5,95 @@ class RecipeCard extends HTMLElement {
   // the element is written into the DOM directly as <recipe-card>
   constructor() {
     super(); // Inheret everything from HTMLElement
-
     // EXPOSE - START (All expose numbers start with A)
     // A1. TODO - Attach the shadow DOM to this Web Component (leave the mode open)
+            let shadowEl=this.attachShadow({mode:'open'});
     // A2. TODO - Create an <article> element - This will hold our markup once our data is set
+            let articleEl=document.createElement('article');
+            articleEl.id="articleId";
     // A3. TODO - Create a style element - This will hold all of the styles for the Web Component
+            let  styleEl=document.createElement('style');
     // A4. TODO - Insert all of the styles from cardTemplate.html into the <style> element you just made
+            styleEl.textContent='* {\n' +
+                '    font-family: sans-serif;\n' +
+                '    margin: 0;\n' +
+                '    padding: 0;\n' +
+                '  }\n' +
+                '\n' +
+                '  a {\n' +
+                '    text-decoration: none;\n' +
+                '  }\n' +
+                '\n' +
+                '  a:hover {\n' +
+                '    text-decoration: underline;\n' +
+                '  }\n' +
+                '\n' +
+                '  article {\n' +
+                '    align-items: center;\n' +
+                '    border: 1px solid rgb(223, 225, 229);\n' +
+                '    border-radius: 8px;\n' +
+                '    display: grid;\n' +
+                '    grid-template-rows: 118px 56px 14px 18px 15px 36px;\n' +
+                '    height: auto;\n' +
+                '    row-gap: 5px;\n' +
+                '    padding: 0 16px 16px 16px;\n' +
+                '    width: 178px;\n' +
+                '  }\n' +
+                '\n' +
+                '  div.rating {\n' +
+                '    align-items: center;\n' +
+                '    column-gap: 5px;\n' +
+                '    display: flex;\n' +
+                '  }\n' +
+                '\n' +
+                '  div.rating>img {\n' +
+                '    height: auto;\n' +
+                '    display: inline-block;\n' +
+                '    object-fit: scale-down;\n' +
+                '    width: 78px;\n' +
+                '  }\n' +
+                '\n' +
+                '  article>img {\n' +
+                '    border-top-left-radius: 8px;\n' +
+                '    border-top-right-radius: 8px;\n' +
+                '    height: 118px;\n' +
+                '    object-fit: cover;\n' +
+                '    margin-left: -16px;\n' +
+                '    width: calc(100% + 32px);\n' +
+                '  }\n' +
+                '\n' +
+                '  p.ingredients {\n' +
+                '    height: 32px;\n' +
+                '    line-height: 16px;\n' +
+                '    padding-top: 4px;\n' +
+                '    overflow: hidden;\n' +
+                '  }\n' +
+                '\n' +
+                '  p.organization {\n' +
+                '    color: black !important;\n' +
+                '  }\n' +
+                '\n' +
+                '  p.title {\n' +
+                '    display: -webkit-box;\n' +
+                '    font-size: 16px;\n' +
+                '    height: 36px;\n' +
+                '    line-height: 18px;\n' +
+                '    overflow: hidden;\n' +
+                '    -webkit-line-clamp: 2;\n' +
+                '    -webkit-box-orient: vertical;\n' +
+                '  }\n' +
+                '\n' +
+                '  p:not(.title),\n' +
+                '  span,\n' +
+                '  time {\n' +
+                '    color: #70757A;\n' +
+                '    font-size: 12px;\n' +
+                '  }';
+
     // A5. TODO - Append the <style> and <article> elements to the Shadow DOM
+      shadowEl.append(articleEl);
+      shadowEl.append(styleEl);
+
   }
 
   /**
@@ -38,14 +120,32 @@ class RecipeCard extends HTMLElement {
   set data(data) {
     // If nothing was passed in, return
     if (!data) return;
-
     // A6. TODO - Select the <article> we added to the Shadow DOM in the constructor
+            let articleEl=this.shadowRoot.getElementById("articleId");
     // A7. TODO - Set the contents of the <article> with the <article> template given in
     //           cardTemplate.html and the data passed in (You should only have one <article>,
     //           do not nest an <article> inside another <article>). You should use Template
     //           literals (tempalte strings) and element.innerHTML for this.
+
+      articleEl.innerHTML=`<img src=${data['imgSrc']}` +
+          `    alt=${data['imgAlt']}>\n` +
+          `  <p class="title">\n` +
+          `    <a href=${data['titleLnk']}>${data['titleTxt']}</a>\n` +
+          `  </p>\n` +
+          `  <p class="organization">${data['organization']}</p>\n` +
+          `  <div class="rating">\n` +
+          `    <span>${data['rating']}</span>\n` +
+          `    <img src="/assets/images/icons/${data['rating']}-star.svg" alt="${data['rating']} stars">\n` +
+          `    <span>(${data['numRatings']})</span>\n` +
+          `  </div>\n` +
+          `  <time>${data['lengthTime']}</time>\n` +
+          `  <p class="ingredients">\n` +
+          `    ${data['ingredients']}\n` +
+          `  </p>`;
+      
   }
 }
 
 // A8. TODO - Define the Class as a customElement so that you can create
 //           'recipe-card' elements
+        customElements.define("recipe-card",RecipeCard);
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,6 +24,12 @@ function getRecipesFromStorage() {
   // A9. TODO - Complete the functionality as described in this function
   //           header. It is possible in only a single line, but should
   //           be no more than a few lines.
+      let recipeArr=localStorage.getItem("recipes");
+      if(!recipeArr)
+      {
+          return [];
+      }
+      return JSON.parse(recipeArr);
 }
 
 /**
@@ -35,10 +41,18 @@ function getRecipesFromStorage() {
  */
 function addRecipesToDocument(recipes) {
   // A10. TODO - Get a reference to the <main> element
+          let mainEl=document.getElementsByTagName("main")[0];
+
   // A11. TODO - Loop through each of the recipes in the passed in array,
   //            create a <recipe-card> element for each one, and populate
   //            each <recipe-card> with that recipe data using element.data = ...
   //            Append each element to <main>
+          for(var i=0;i<recipes.length;i++)
+          {
+            let recipeEl=document.createElement("recipe-card");
+            recipeEl.data=recipes[i];
+            mainEl.append(recipeEl);
+          }
 }
 
 /**
@@ -51,6 +65,7 @@ function saveRecipesToStorage(recipes) {
   // B1. TODO - Complete the functionality as described in this function
   //            header. It is possible in only a single line, but should
   //            be no more than a few lines.
+    localStorage.setItem("recipes",JSON.stringify(recipes));
 }
 
 /**
@@ -60,26 +75,45 @@ function saveRecipesToStorage(recipes) {
 function initFormHandler() {
 
   // B2. TODO - Get a reference to the <form> element
-  
+  let formEl=document.getElementById("new-recipe");
   // B3. TODO - Add an event listener for the 'submit' event, which fires when the
   //            submit button is clicked
+    formEl.addEventListener("submit",(event)=>{
+        // Steps B4-B9 will occur inside the event listener from step B3
+        // B4. TODO - Create a new FormData object from the <form> element reference above
+        let formData=new FormData(formEl);
+        // B5. TODO - Create an empty object (I'll refer to this object as recipeObject to
+        //            make this easier to read), and then extract the keys and corresponding
+        //            values from the FormData object and insert them into recipeObject
+        let recipeObject={};
+        for (const [key, value] of formData) {
+            recipeObject[key]=value;
+        }
+        // B6. TODO - Create a new <recipe-card> element
+        let recipeCard=document.createElement("recipe-card");
+        // B7. TODO - Add the recipeObject data to <recipe-card> using element.data
+        recipeCard.data=recipeObject;
+        // B8. TODO - Append this new <recipe-card> to <main>
+        document.getElementsByTagName("main")[0].append(recipeCard);
+        // B9. TODO - Get the recipes array from localStorage, add this new recipe to it, and
+        //            then save the recipes array back to localStorage
+        let recipes = getRecipesFromStorage();
+        recipes.push(recipeObject);
+        saveRecipesToStorage(recipes);
+    });
 
-  // Steps B4-B9 will occur inside the event listener from step B3
-  // B4. TODO - Create a new FormData object from the <form> element reference above
-  // B5. TODO - Create an empty object (I'll refer to this object as recipeObject to
-  //            make this easier to read), and then extract the keys and corresponding
-  //            values from the FormData object and insert them into recipeObject
-  // B6. TODO - Create a new <recipe-card> element
-  // B7. TODO - Add the recipeObject data to <recipe-card> using element.data
-  // B8. TODO - Append this new <recipe-card> to <main>
-  // B9. TODO - Get the recipes array from localStorage, add this new recipe to it, and
-  //            then save the recipes array back to localStorage
 
   // B10. TODO - Get a reference to the "Clear Local Storage" button
+    let buttonEl=document.getElementsByTagName("button")[1];
   // B11. TODO - Add a click event listener to clear local storage button
-  
-  // Steps B12 & B13 will occur inside the event listener from step B11
-  // B12. TODO - Clear the local storage
-  // B13. TODO - Delete the contents of <main>
+    buttonEl.addEventListener("click",(event)=>{
+        // Steps B12 & B13 will occur inside the event listener from step B11
+        // B12. TODO - Clear the local storage
+        localStorage.clear();
+        // B13. TODO - Delete the contents of <main>
+        document.getElementsByTagName("main")[0].innerHTML="";
+
+    });
+
 
 }
